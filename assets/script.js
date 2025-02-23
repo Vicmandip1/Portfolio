e2d3748fc0f464e8b7e906b843d2b4fa1d0d4fc3
Victor S. Dipeolu <158380224+Vicmandip1@users.noreply.github.com>
2025-02-23 08:07:41 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
+// Function to Toggle Details and Change Button Text
 function toggleDetails(id) {
-  console.log("Toggle button clicked for: " + id); // Debugging line
-  const details = document.getElementById(id);
-  if (details.style.display === "none" || details.style.display === "") {
-    details.style.display = "block";
-  } else {
-    details.style.display = "none";
-  }
+    console.log("Toggle button clicked for: " + id); // Debugging line
+    const details = document.getElementById(id);
+    const button = details.previousElementSibling;
+
+    // Check current display state of the details section
+    if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+        button.innerText = "See Less";
+    } else {
+        details.style.display = "none";
+        button.innerText = "More";
+    }
 }
